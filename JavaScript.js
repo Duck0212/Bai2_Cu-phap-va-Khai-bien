c351bdebbb5b82e65eb2b9ce9a7b89a01480d8e2
Duck0212 <tranhoangduc0212@gmail.com>
2025-10-01 10:32:28 +0700

    add

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+//Khai biến các thông tin
+let name = "Trần Hoàng Đức" // kiểu string
+birthYear = 2000        // kiểu number
+gender = "Nam"          // kiểu string
+//Khai biến năm hiện tại
+const   CURRENT_YEAR = 2025;
